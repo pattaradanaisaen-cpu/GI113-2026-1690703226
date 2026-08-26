9560593027cb7eb602b7f53a73c1809665f0c5eb
pattaradanaisaen-cpu <pattaradanai.saen@bumail.net>
2026-08-26 18:06:17 +0700

    Add lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID :1690703226
+ * Name       : Lab02
+ * Section    :129C
+ * No.        :n/a
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
